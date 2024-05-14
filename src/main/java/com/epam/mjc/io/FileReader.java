76a37fada8c68cd7f56dd58d76a3ca6e0a1f2f65
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-05-14 09:01:50 +0500

    NOT FINISHED YET

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,21 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
 
-
 public class FileReader {
-
+    final private static String File_PATH = "src/main/resources/Profile.txt";
     public Profile getDataFromFile(File file) {
+        try{
+            BufferedReader reader = new BufferedReader(new java.io.FileReader(File_PATH));
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+            reader.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return new Profile();
     }
 }
